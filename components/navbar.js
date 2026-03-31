1678a50ee5368391cc48d3efa06a2835bc9e14aa
syiar <syiaraltarif@gmail.com>
2026-03-31 09:27:19 +0800

    hari 2: tambah halaman dan navbar

--- components/navbar.js
+++ components/navbar.js
@@ -15,20 +15,22 @@
 //     </nav>
 //   )
 // }
-import link from 'next/link'
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <nav style={{ 
-            display: 'flex',
-            gap: '24px',
-            padding: '16px 24px',
-            borderBottom: '1px solid #e5e7eb',
-            backgroundColor: '#fff',
-         }}>
-            <link href='/dashboard'>Dashboard</link>
-            <link href='/transactions'>Transactions</link>
-            <link href='/login'>Login</link>
-         </nav>
-    )
+  return (
+    <nav
+      style={{
+        display: "flex",
+        gap: "24px",
+        padding: "16px 24px",
+        borderBottom: "1px solid #e5e7eb",
+        backgroundColor: "#fff",
+      }}
+    >
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/transactions">Transactions</Link>
+      <Link href="/login">Login</Link>
+    </nav>
+  );
 }
